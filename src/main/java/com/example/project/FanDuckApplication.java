cce7255f5f4500bf20f0af5781a81d401663af1d
YJiHyeon <qazzaq.00.wlgus@gmail.com>
2023-11-26 13:05:33 +0900

    project 설정

---
+++ src/main/java/com/example/project/FanDuckApplication.java
@@ -0,0 +1,13 @@
+package com.example.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanDuckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FanDuckApplication.class, args);
+	}
+
+}
